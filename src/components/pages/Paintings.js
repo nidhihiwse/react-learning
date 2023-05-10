936fe76703dbf5fbea2b67071933db56c97eb5fb
nidhihiwse <hiwse.nid@gmail.com>
2023-05-10 16:51:50 +0530

    api request

--- src/components/pages/Paintings.js
+++ src/components/pages/Paintings.js
@@ -3,35 +3,50 @@
 import './paintings.css';
 
 import CardList from "../../cards/CardList";
+import { useState, useEffect } from "react";
 
 
 function PaintingsPage() {
-  const DUMMY_DATA = [
-    {
-      id: "01",
-      title: "ROBIN'S RESORT",
-      image: "https://images.saatchiart.com/saatchi/710107/art/10161621/9224387-ZRKWAOPE-6.jpg",
-      description: "Paintings, 91.4 W x 61 H x 2.5 D cm Tatiana Iliina"
-    },
-    {
-      id: "02",
-      title: "Spent Horses",
-      image: "https://images.saatchiart.com/saatchi/185414/art/3526333/2596220-KVOJOOID-6.jpg",
-      description: "Paintings, 203.2 W x 188 H x 5.1 D cm Matthew Dibble"
-    },
-    {
-      id: "03",
-      title: "Todd Mires",
-      image: "https://images.saatchiart.com/saatchi/837389/art/9680821/8743921-GUDIAUDC-6.jpg",
-      description: "Paintings, 182.9 W x 91.4 H x 3.8 D cm"
-    },
-  ]
+  const [isLoading, setIsLoading] = useState(true);
+  const [dataLoaded, setDataLoaded] = useState([]);
+
+  useEffect(() => {
+    console.log("componentDidMount");
+    setIsLoading(true);
+    fetch('https://react-practice-5da0e-default-rtdb.asia-southeast1.firebasedatabase.app/cards.json'
+    ).then(response => {
+      return response.json();
+    }).then(data => {
+        const cards = [];
+        for(const key in data) {
+          const card = {
+            id: key,
+            ...data[key]
+          };
+          cards.push(card);
+        }
+        setIsLoading(false);
+        setDataLoaded(cards);
+    });
+    // return () => {
+    //   console.log("unmounted");
+    // }
+  }, []);
+
+  if(isLoading) {
+    return (
+    <div className="loading-section">
+      <img alt="" src="./loading.gif" />
+    </div>
+    )
+  }
+
   return (
     <section className="section-wrapper">
       <h1><i>“Painting is just another way of keeping a diary”</i></h1>
       <div className="gallery-container">
         <div className="gallery">
-        <CardList cards={DUMMY_DATA}/>
+        <CardList cards={dataLoaded}/>
         </div>
       </div>  
     </section>
